a7e39c556f5c2173a421304fd64f29e7ef5375b7
MammaGula <supaphit.rue2@gmail.com>
2026-02-23 19:55:07 +0100

    Add project files.

---
+++ IntelliAssist/Models/UserMovie.cs
@@ -0,0 +1,11 @@
+﻿namespace IntelliAssist.Models
+{
+
+    public class UserMovie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public int Rating { get; set; }
+    }
+}
